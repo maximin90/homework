4fd2bf77fd7025723b4e1f7fa21ec9ac80cb3598
maximin90 <85061329+maximin90@users.noreply.github.com>
2021-10-02 16:23:39 +0900

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -8,9 +11,19 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-# 아래 빈 칸('')을 채워보세요
+docs = []
+
 for tr in trs:
     rank = tr.select_one('td.number').text[0:2].strip()
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     print(rank, title, artist)
+# DB 추가
+    doc = {
+        "rank" : rank,
+        "title" : title,
+        "artist" : artist,
+    }
+    docs.append(doc)
+
+db.genie.insert_many(docs):
